993d6215a93c545ff631d536772ed4bf531ddc2d
ebhoom1 <info.ebhoom@gmail.com>
2025-01-29 12:17:15 +0530

    changed image to jpg

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -333,7 +333,7 @@ const Home = () => {
 
               <div className="overflow-hidden rounded-lg shadow-lg">
                 <img
-                  src="/assets/images/grid3img.jpeg"                
+                  src="/assets/images/grid3img.jpg"                
                   alt="Office View 3"
                   className="w-full h-[200px] md:h-[300px] object-cover"
                 />
